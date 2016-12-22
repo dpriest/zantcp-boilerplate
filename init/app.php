d593f6642cc189a5fb62c477c94ced9f06eb0aa9
zhangwenhao <zhangwenhao@youzan.com>
2016-12-22 20:56:57 +0800

    a

---
+++ init/app.php
@@ -0,0 +1,7 @@
+<?php
+use Zan\Framework\Foundation\Application;
+require __DIR__ . '/../vendor/autoload.php';
+$appName = '{{APP_NAME}}';
+$rootPath = realpath(__DIR__.'/../');
+$app = new Application($appName, $rootPath);
+return $app;
